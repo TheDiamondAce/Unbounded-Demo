83bdb348af5b2edea9441424de8ac924df7476d0
TheDiamondAce <75910142+TheDiamondAce@users.noreply.github.com>
2026-02-06 20:16:53 -0800

    deleted GUIDE, made priority keybinds
    So earlier problem was that the combo of key strokes werent working so what we did to fix that was to implement else if statments to the keybind list.

--- Script/PlayerController.gd
+++ Script/PlayerController.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var animSprite = $Rai_Animation 
-@export var move:GUIDEAction
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +11,6 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	_animation()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -21,27 +19,44 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
+		print("moving")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
+	animation()
+	move_and_slide()	
 
-func _animation() -> void:
-	if Input.is_action_pressed("duck") and is_on_floor(): 
-		animSprite.play("duck")
+func animation() -> void:
+	#attack animation
 	if Input.is_action_just_pressed("attack"):
-		animSprite.play("punch")
-	if Input.is_action_pressed("attack") && Input.is_action_pressed("duck"):
-		animSprite.play("hook")
-	if Input.is_action_pressed("weave"):
-		animSprite.play("weave")
-		
-	if Input.is_action_pressed("attack") && Input.is_action_pressed("weave"):
-		animSprite.play("kick")
-	if !Input.is_anything_pressed():
-		animSprite.play("idle")
-		
+			animSprite.play("punch")
 
-	move_and_slide()
+	#duck animations
+	if Input.is_action_pressed("duck"):
+		if Input.is_action_just_pressed("attack"):
+			animSprite.play("hook")
+		else: if Input.is_action_pressed("dodge_left"):
+			animSprite.play("duck_left")
+		else: if Input.is_action_pressed("dodge_right"):
+			animSprite.play("duck_right")
+		else:
+			animSprite.play("duck")
+		
+	#weave animations		
+	if Input.is_action_pressed("weave"):
+		if Input.is_action_just_pressed("attack"):
+			animSprite.play("kick")
+		else: if Input.is_action_pressed("dodge_left"):
+			animSprite.play("weave_left")
+		else: if Input.is_action_pressed("dodge_right"):
+			animSprite.play("weave_right")
+		else:
+			animSprite.play("weave")		
+	# idle animations
+	else: if !Input.is_anything_pressed():
+		animSprite.play("idle")
+	 
+	
