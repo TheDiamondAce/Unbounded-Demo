406d9496aede4f5a37a6ce27bba60fa710a2288e
TheDiamondAce <75910142+TheDiamondAce@users.noreply.github.com>
2026-02-11 11:18:51 -0800

    creation of combo creator
    made a combo creator to make combo's more easier to make.

---
+++ Scene/combo_creator.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+var inputSequence = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("left"):
+		record_input("left")
+	if Input.is_action_just_pressed("right"):
+		record_input("right")
+	if Input.is_action_just_pressed("jump"):
+		record_input("jump")
+	if Input.is_action_just_pressed("dash"):
+		record_input("dash")
+	if Input.is_action_just_pressed("attack"):
+				record_input("punch")
+
+	#duck animations
+	if Input.is_action_pressed("duck"):
+		if Input.is_action_just_pressed("attack"):
+			record_input("hook")
+		else: if Input.is_action_just_pressed("dodge_left"):
+			record_input("duck_left")
+		else: if Input.is_action_just_pressed("dodge_right"):
+			record_input("duck_right")
+		else: if Input.is_action_just_pressed("duck"):
+			record_input("duck")
+		
+	#weave animations		
+	if Input.is_action_pressed("weave"):
+		if Input.is_action_just_pressed("attack"):
+			record_input("kick")
+		else: if Input.is_action_just_pressed("dodge_left"):
+			record_input("weave_left")
+		else: if Input.is_action_just_pressed("dodge_right"):
+			record_input("weave_right")
+		else: if Input.is_action_just_pressed("weave"):
+			record_input("weave")
+		pass
+		
+		
+func record_input(action):
+		inputSequence.append(action)
+		print(action)
+		print(inputSequence)
+		if inputSequence.size() == 4:
+			inputSequence =  []
